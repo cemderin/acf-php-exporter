99007c40e02f586f5b1d862789d5da8f13e02e37
Cem Derin <cem@wysiwyg.de>
2019-11-15 10:55:57 +0100

    Fix descriptions

--- acf-php-exporter.php
+++ acf-php-exporter.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: ACF PHP Exporter
  * Plugin URI: https://github.com/wysiwyg-software-design/acf-php-exporter
- * Description: PHP exporter for ACF field groups
+ * Description: PHP exporter for ACF groups
  * Version: 1.1.0
  * Author: Cem Derin (wysiwyg software design GmbH)
  * Author URI: https://wysiwyg.de
